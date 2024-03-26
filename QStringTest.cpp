8f5c8945a3170bf067e95a54ba0821e3a3883bee
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-03-26 14:10:18 +0900

    improve 5

--- QStringTest.cpp
+++ QStringTest.cpp
@@ -66,8 +66,10 @@ void speed_test_string(void)
     });
 }
 
-int main(void)
+void test_QStringA(void)
 {
+    std::cout << "test_QStringA" << std::endl;
+
     QStringA str1("Hello");
     assert(str1.size() == 5);
     assert(strcmp(str1.c_str(), "Hello") == 0);
@@ -120,6 +122,70 @@ int main(void)
     QStringA fmt;
     fmt.format("%s, %d", "Number", 42);
     assert(strcmp(fmt.c_str(), "Number, 42") == 0);
+}
+
+void test_string(void)
+{
+    std::cout << "test_string" << std::endl;
+
+    std::string str1("Hello");
+    assert(str1.size() == 5);
+    assert(strcmp(str1.c_str(), "Hello") == 0);
+
+    std::string str2(str1);
+    assert(str1 == str2);
+
+    str2.clear();
+    assert(str2.empty());
+
+    str2 = "World";
+    assert(str2.size() == 5);
+    assert(strcmp(str2.c_str(), "World") == 0);
+
+    str2 += '!';
+    assert(str2.size() == 6);
+    assert(strcmp(str2.c_str(), "World!") == 0);
+
+    str2.insert(5, " Hello");
+    assert(strcmp(str2.c_str(), "World Hello!") == 0);
+
+    str2.erase(5, 6);
+    assert(strcmp(str2.c_str(), "World!") == 0);
+
+    str2.replace(0, 5, "Goodbye");
+    assert(strcmp(str2.c_str(), "Goodbye!") == 0);
+
+    assert(str1.compare("Hello") == 0);
+    assert(str1.compare("World") < 0);
+    assert(str1.compare("Apple") > 0);
+
+    assert(str1.icompare("hello") == 0);
+    assert(str1.icompare("WORLD") < 0);
+    assert(str1.icompare("apple") > 0);
+
+    assert(str1.find('e') == 1);
+    assert(str1.find("lo") == 3);
+    assert(str1.rfind('l') == 3);
+    assert(str1.rfind("el") == 1);
+    assert(str1.ifind('E') == 1);
+    assert(str1.ifind("LO") == 3);
+    assert(str1.find("llo") == 2);
+    assert(str1.find("LLO") == str1.npos);
+    assert(str1.ifind("LLO") == 2);
+    assert(str1.rfind("l") == 3);
+
+    std::string sub = str1.substr(1, 3);
+    assert(strcmp(sub.c_str(), "ell") == 0);
+
+    //std::string fmt;
+    //fmt.format("%s, %d", "Number", 42);
+    //assert(strcmp(fmt.c_str(), "Number, 42") == 0);
+}
+
+int main(void)
+{
+    test_QStringA();
+    test_string();
 
     speed_test_CStringA();
     speed_test_string();
